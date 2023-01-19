eca7effc0a55e0b298faa57feec5089b13607ef3
Jenny Roques <90016446+jenroques@users.noreply.github.com>
2023-01-18 20:08:16 -0600

    Added resouces to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  
+
+  resources :shops, only: [:index, :show]
+  resources :reviews, only: [:index, :show, :create, :update, :destroy]
+  resources :users, only: [:index, :show, :create, :update, :destroy]
+  resources :favorites, only: [:index, :show, :create, :destroy]
+
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
