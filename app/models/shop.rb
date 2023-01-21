e89a2574450617383822e2031a6b89a3a5cc5e74
Jenny Roques <90016446+jenroques@users.noreply.github.com>
2023-01-21 15:54:27 -0600

    Added calculate_average method

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -3,7 +3,13 @@ class Shop < ApplicationRecord
   has_many :users, through: :reviews
   has_many :favorites, through: :users
 
-  validates :id, presence: :true
   validates :name, presence: :true
+
+  def calculate_average
+    return 0 unless reviews.size.positive?
+
+    avg = reviews.average(:score).to_f.round(2)
+    update_column(:average_score, avg)
+  end
 
 end
