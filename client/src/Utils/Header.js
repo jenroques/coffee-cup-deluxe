c76f71e99609c803c31b124932aa023c1bbc79db
Jenny Roques <90016446+jenroques@users.noreply.github.com>
2023-01-21 16:01:08 -0600

    Create Header JS

---
+++ client/src/Utils/Header.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  padding:100px 100px 10px 100px;
+
+  h1 {
+    font-size:42px;
+  }
+`
+
+const Subheader = styled.p`
+  font-weight:300;
+  font-size:26px;
+`
+
+const Header = () => {
+    // Add Animation
+
+    return (
+        <Wrapper>
+            <h1>The Coffee Cup</h1>
+            <Subheader>Your coffee shop headquarters. Share your experience.</Subheader>
+        </Wrapper>
+    )
+}
+
+export default Header
