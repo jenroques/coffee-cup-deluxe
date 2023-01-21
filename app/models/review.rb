5a1f58dc1a6370c8995d85b3a5ed2b0c73dec98b
Jenny Roques <90016446+jenroques@users.noreply.github.com>
2023-01-21 15:54:10 -0600

    Added average_score and created new model because of DB issues

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,15 +4,7 @@ class Review < ApplicationRecord
 
   validates :title, presence: :true
   validates :description, presence: :true
-  validates :id, presence: :true
-  validate :review_length
 
-  def review_length
-    if review.length < 5
-        errors.add(:review, "Review is too short.")
-    elsif review.length > 250
-        errors.add(:review, "Review is too long.")
-    end
-  end
+  after_commit -> { shop.calculate_average }
 
 end
