aea31994efb8897a6ae1da3d674e5252ffcc2749
Jenny Roques <90016446+jenroques@users.noreply.github.com>
2023-01-22 10:10:42 -0600

    Added session routes get post and delete.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   resources :reviews, only: [:index, :show, :create, :update, :destroy]
   resources :users, only: [:index, :show, :create, :update, :destroy]
   resources :favorites, only: [:index, :show, :create, :destroy]
+  get "/cookie_click", to: "sessions#click"
+  get "/me", to: "users#show"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
