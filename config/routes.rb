b6e0c63b0b55797f890a031d520920add44b00be
Jenny Roques <90016446+jenroques@users.noreply.github.com>
2023-01-22 11:14:56 -0600

    Added auth routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,11 @@ Rails.application.routes.draw do
 
   resources :shops, only: [:index, :show]
   resources :reviews, only: [:index, :show, :create, :update, :destroy]
-  resources :users, only: [:index, :show, :create, :update, :destroy]
+  resources :users, only: [:create, :update, :destroy]
   resources :favorites, only: [:index, :show, :create, :destroy]
-  get "/cookie_click", to: "sessions#click"
-  get "/me", to: "users#show"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
+  get "/auth", to: "users#show"
 
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
