97bf33b0fb054bee0943335e66130cda1ba3e122
Jenny Roques <90016446+jenroques@users.noreply.github.com>
2023-01-25 21:04:56 -0600

    Created Auth

--- client/src/User/Auth.js
+++ client/src/User/Auth.js
@@ -1,34 +1,8 @@
 import React from 'react'
 
-const Auth = ({ setCurrentUser }) => {
-    const [name, setName] = useState('');
-    const [password, setPassword] = useState('');
-    const [login, setLogin] = useState('');
-    const [errors, setErrors] = useState([]);
-
-    const onSubmit = (e) => {
-        e.preventDefault();
-        const user = {
-            name,
-            password
-        }
-        fetch('/users', {
-            method: "POST",
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(user)
-        })
-            .then(res => {
-                if (res.ok) {
-                    res.json().then(setCurrentUser)
-                } else {
-                    res.json().then(e => setErrors(e.error))
-                }
-            })
-    }
-
-
+const Auth = ({ user }) => {
     return (
-        <div>Auth</div>
+        <div>{`Logged In: ${user.name}`}</div>
     )
 }
 
