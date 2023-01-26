348c0ad4527c3d4aac86dfb631b273d22bf1bb11
Jenny Roques <90016446+jenroques@users.noreply.github.com>
2023-01-25 21:02:47 -0600

    corrected routes and updated

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Rails.application.routes.draw do
 
   resources :shops, only: [:index, :show]
   resources :reviews, only: [:index, :show, :create, :update, :destroy]
-  resources :users, only: [:create, :update, :destroy]
-  resources :favorites, only: [:index, :show, :create, :destroy]
+  post '/addreview', to: "reviews#create"
+  get "/me", to: "users#show"
+  post "/signup", to: "users#create"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
-  get "/auth", to: "users#show"
+
+  #get "/auth", to: "users#show"
 
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
