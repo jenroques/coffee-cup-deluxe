823047fde24c4046d1e7d75960a73d2821f2201b
Jenny Roques <90016446+jenroques@users.noreply.github.com>
2023-01-29 09:58:16 -0600

    Added form and functionality to create new shops.

--- client/src/Shops/Shops.js
+++ client/src/Shops/Shops.js
@@ -1,8 +1,9 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import Shop from './Shop';
 import Header from '../Utils/Header';
 import styled from 'styled-components';
 import { Context } from '../Utils/Context';
+import { Box, Button, TextField, Modal } from '@mui/material';
 
 const Home = styled.div`
   text-align:center;
@@ -24,33 +25,148 @@ const Grid = styled.div`
   }
 `
 
-const Shops = ({ user, shops, shop, setShop }) => {
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 250,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
-    console.log(shops)
+const Shops = ({ user, shops, setShop }) => {
+  const { setShops } = useContext(Context)
+  const [errors, setErrors] = useState([])
+  const [newShop, setNewShop] = useState()
+  const [isLoading, setIsLoading] = useState(false)
+  const [open, setOpen] = useState(false)
 
-    const grid = shops.map((shop, index) => {
-        const { id, name, image_url } = shop
+  const defaultValues = {
+    name: "",
+    image_url: "",
+  }
 
-        return (
-            <Shop
-                key={index}
-                name={name}
-                image_url={image_url}
-                id={id}
-                user={user}
-                shop={shop}
-                setShop={setShop}
-            />
-        )
-    })
+  const [form, setForm] = useState(defaultValues)
+  console.log(shops)
 
+  const handleClose = () => {
+    setOpen(false);
+  }
+
+  const handleClick = () => {
+    setOpen(true);
+  }
+
+  function handleChange(e) {
+    let name = e.target.name;
+    let value = e.target.value;
+    setForm({ ...form, [name]: value });
+  }
+
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setErrors([])
+    fetch("/shops", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(form),
+    }).then((res) => {
+      if (res.ok) {
+        res.json().then((shop) => {
+          setShops([...shops, shop])
+          shopCallbackHandle(shop);
+        });
+      } else {
+        res.json().then((err) => setErrors(err.error));
+      }
+      setForm(defaultValues);
+      setNewShop(shops)
+      setIsLoading(false)
+    });
+    handleClose();
+  }
+
+  const shopCallbackHandle = async () => {
+    fetch('/shops')
+      .then((res) => res.json())
+      .then((newShop) => setNewShop(newShop))
+  }
+
+  const grid = shops.map((shop, index) => {
+    const { id, name, image_url } = shop
 
     return (
-        <Home>
-            <Header />
-            <Grid>{grid}</Grid>
-        </Home>
+      <Shop
+        key={index}
+        name={name}
+        image_url={image_url}
+        id={id}
+        user={user}
+        shop={shop}
+        setShop={setShop}
+      />
     )
+  })
+
+
+  return (
+    <div>
+      <Home>
+        <Header />
+        <Grid>{grid}</Grid>
+      </Home>
+      <div>
+        <Button onClick={handleClick}>Add Shop</Button>
+        <Modal
+          keepMounted
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-review"
+          aria-describedby="modal-review"
+        >
+          <div>
+            <Box
+              component="form"
+              sx={style}
+              noValidate
+              autoComplete="off"
+              onSubmit={handleSubmit}
+            >
+              <TextField
+                id="shop-name"
+                defaultValue="Name"
+                label="Name"
+
+                onChange={handleChange}
+                value={form.name}
+                name='name'
+              />
+              <div>
+                <TextField
+                  sx={{ mt: 2 }}
+                  id="shop_image"
+                  label="Image URL"
+                  defaultValue="Image URL"
+                  onChange={handleChange}
+                  value={form.image_url}
+                  name='image_url'
+                />
+              </div>
+              <Button type="submit">Create Shop</Button>
+              <Button onClick={handleClose}>Cancel</Button>
+
+            </Box>
+          </div>
+        </Modal>
+
+      </div> </div>
+  )
 }
 
 export default Shops
