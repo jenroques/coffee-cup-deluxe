96976ff429ac274361509d0badbd6527764c033b
Jenny Roques <90016446+jenroques@users.noreply.github.com>
2023-01-28 20:01:46 -0600

    Cleaned up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-
+  resources :users, only: [:show, :create]
   resources :shops, only: [:index, :show]
   resources :reviews, only: [:index, :show, :create, :update, :destroy]
-  post '/addreview', to: "reviews#create"
-  get "/me", to: "users#show"
   post "/signup", to: "users#create"
   post "/login", to: "sessions#create"
+  get "/me", to: "users#show"
   delete "/logout", to: "sessions#destroy"
 
   #get "/auth", to: "users#show"
