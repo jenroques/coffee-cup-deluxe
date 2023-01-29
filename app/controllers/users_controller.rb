4d0fbadf3cb9a708cb9ac0f1cff158ec01f27e5a
Jenny Roques <90016446+jenroques@users.noreply.github.com>
2023-01-28 19:55:07 -0600

    Updated skip_before_action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  rescue_from ActiveRecord::RecordInvalid, with: :render_invalid_record
-  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
-  #skip_before_action :authorize, only: :create
+   rescue_from ActiveRecord::RecordInvalid, with: :render_invalid_record
+   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+   #skip_before_action :authorize, only: :create :show
 
   def show
     user = User.find(session[:user_id])
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
 
   def create
     user = User.create!(user_params)
-    session[:user_id]=user.id
+    session[:user_id]= user.id
     render json: user, status: :created
   end
 
@@ -22,10 +22,6 @@ class UsersController < ApplicationController
 
   def user_params
     params.permit(:name, :password, :password_confirmation)
-  end
-
-  def password_update
-    params.permit(:password)
   end
 
   def render_not_found_response
